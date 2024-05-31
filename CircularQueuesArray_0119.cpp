b4b1729c34a0348aab38aa92152bbed663a6f320
kiieeaa <kiyaadinda5@gmail.com>
2024-05-31 10:43:06 +0700

    cek jika antrian hanya memiliki satu elemen

--- CircularQueuesArray_0119.cpp
+++ CircularQueuesArray_0119.cpp
@@ -38,4 +38,18 @@ public: Queues() {
 		  queue_array[REAR] = num;
 	  }
 
+	  void remove() {
+
+		  if (FRONT == -1) {
+			  cout << "Queue underflow\n";
+			  return;
+		  }
+		  cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+
+		  if (FRONT == REAR) {
+			  FRONT = -1;
+			  REAR = -1;
+		  }
+		  else{
 
